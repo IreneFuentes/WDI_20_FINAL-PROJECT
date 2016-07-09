f2072b8cdcab0a9735962507d1c9319513930d06
Irene Fuentes <fuentes.acedo1.@gmail.com>
2016-07-09 17:21:45 +0100

    added paperclip for avatars

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+
 gem 'rails', '4.2.6'
 gem 'pg', '~> 0.15'
 gem 'sass-rails', '~> 5.0'
@@ -10,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'high_voltage'
 gem 'devise'
+gem "paperclip"
 
 group :development, :test do
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-128.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
