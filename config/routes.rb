88fd8502ff9d6bae3dc2e4661c517d1d2137f6d0
Irene Fuentes <fuentes.acedo1.@gmail.com>
2016-07-09 13:32:37 +0100

    Added HighVoltage to handle static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'high_voltage/pages#show', id: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
